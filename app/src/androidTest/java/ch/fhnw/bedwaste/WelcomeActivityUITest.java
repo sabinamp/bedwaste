88d43c04a065ad7f45ea35e600a7371da8d2bb7b
Anthony <anthony.kalbermatten@students.fhnw.ch>
2019-05-08 11:44:28 +0200

    Adjusted build.gradle so that TestRules are recognised, UI Tests don't work yet but show filter did on the other repo, will look into that

--- app/src/androidTest/java/ch/fhnw/bedwaste/WelcomeActivityUITest.java
+++ app/src/androidTest/java/ch/fhnw/bedwaste/WelcomeActivityUITest.java
@@ -1,19 +1,26 @@
 package ch.fhnw.bedwaste;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
-public class WelcomeActivityTest {
+public class WelcomeActivityUITest {
     @Test
     public void getDeviceLocationTest () {
 
 
 
+    }
+
+    @RunWith(AndroidJUnit4.class)
+    public static class WelcomeActivityTest {
+        @Test
+        public void getDeviceLocationTest () {
+
+
+
+        }
     }
 }
